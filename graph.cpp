24a19b3841be9673961e9cd54900ef4fdde6d4e1
Dmitry Vetrov <rtpurtooe22@mail.ru>
2024-05-08 11:40:57 +0300

    ii

--- graph.cpp
+++ graph.cpp
@@ -3,6 +3,13 @@
 Graph::Graph(QWidget* parent) : QWidget(parent)
 {
     list = listNew();
+    metrics = {
+        .minimumYear = 0,
+        .maximumYear = 0,
+        .minimum = 0,
+        .maximum = 0,
+        .median = 0,
+    };
 }
 
 Graph::~Graph() {
@@ -21,27 +28,70 @@ void Graph::draw(DemographList* list, Metrics* metrics, int column) {
     repaint();
 }
 
+int Graph::graphHeight() {
+    return height() - 30;
+}
+
+int Graph::graphWidth() {
+    return width() - 70;
+}
+
+double Graph::Mx() {
+    return (double) graphWidth() / (metrics.maximumYear - metrics.minimumYear);
+}
+
+double Graph::My() {
+    return ((double) graphHeight() / (metrics.maximum - metrics.minimum + 2));
+}
+
 QPoint Graph::getPoint(Node* node) {
-    double Mx = (double) width() / (metrics.maximumYear - metrics.minimumYear);
-    double My = ((double) height() / (metrics.maximum - metrics.minimum));
     return QPoint{
-        (int) (Mx * (node->record.year - metrics.minimumYear)),
-        height() - (int)(My * (getColumnByIndex(&node->record, column) - metrics.minimum))
+        (width() - graphWidth()) + (int) (Mx() * (node->record.year - metrics.minimumYear)),
+        graphHeight() - (int)(My() * (getColumnByIndex(&node->record, column) - metrics.minimum + 1)),
     };
+}
+
+void Graph::drawGraph(QPainter &painter) {
+    Node* node = list.first ? (list.first->next) : 0;
+    while (node) {
+        QPoint current = getPoint(node);
+        QPoint previous = node->prev ? getPoint(node->prev) : QPoint{0, 0};
+        painter.setPen(Qt::black);
+        painter.drawLine(previous, current);
+        painter.setPen(QPen(Qt::gray, 1, Qt::DashDotLine));
+        node = node->next;
+    }
+}
+
+void Graph::drawAxis(QPainter &painter) {
+    painter.setPen(QPen(Qt::black, 1, Qt::DashDotLine));
+    painter.drawLine(0, graphHeight(), width(), graphHeight());
+    for (int x = metrics.minimumYear; x <= metrics.maximumYear; x++) {
+        double xAxis = (int) (Mx() * (x - metrics.minimumYear)) + width() - graphWidth();
+        painter.setPen(Qt::black);
+        painter.drawText(xAxis - 35, height() - 10, QString::number(x));
+        painter.setPen(QPen(Qt::lightGray, 1, Qt::DashDotLine));
+        painter.drawLine(xAxis, 0, xAxis, graphHeight());
+    }
+    for (int y = metrics.minimum - 1; y <= metrics.maximum + 1; y++) {
+        double yAxis = graphHeight() - (int)(My() * (y - metrics.minimum + 1));
+        if (yAxis > graphHeight()) {
+            continue;
+        }
+        painter.setPen(Qt::black);
+        painter.drawText(width() - graphWidth() - 30, yAxis - 5, QString::number(y));
+        painter.setPen(QPen(Qt::lightGray, 1, Qt::DashDotLine));
+        painter.drawLine((int) (width() - graphWidth()), yAxis, width(), yAxis);
+    }
 }
 
 void Graph::paintEvent(QPaintEvent* ev) {
     QPainter painter(this);
     painter.setPen(Qt::black);
-    painter.drawRect(0, 0, width() - 1, height() - 1);
     if (metrics.maximumYear - metrics.minimumYear != 0) {
-        Node* node = list.first ? (list.first->next) : 0;
-        while (node) {
-            QPoint current = getPoint(node);
-            QPoint previous = node->prev ? getPoint(node->prev) : QPoint{0, 0};
-            painter.drawLine(previous, current);
-            node = node->next;
-        }
+        drawAxis(painter);
+        drawGraph(painter);
     }
+    painter.drawRect(0, 0, width() - 1, height() - 1);
 }
 
--- graph.h
+++ graph.h
@@ -1,6 +1,7 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-#include "QGraphicsView"
+#include <QPainter>
+#include <QWidget>
 #include "list.h"
 #include "metrics.h"
 
@@ -10,12 +11,21 @@ private:
     DemographList list;
     Metrics metrics;
     int column;
+
+    double Mx();
+    double My();
+    QPoint getPoint(Node* node);
+
+    void drawAxis(QPainter& painter);
+    void drawGraph(QPainter& painter);
+
+    int graphHeight();
+    int graphWidth();
 public:
     Graph(QWidget* parent = 0);
     ~Graph();
 
     void draw(DemographList* list, Metrics* metrics, int column);
-    QPoint getPoint(Node* node);
 
     void paintEvent(QPaintEvent *event) override;
 };
